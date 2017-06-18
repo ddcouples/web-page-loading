15b55fd87f2870346ab703230bc9c7193e3bac93
ddcouples <ddcouples>
2017-06-18 11:15:08 +0800

    module.exports

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-import Loading from '../lib/Loading';
-
-var dom=document.querySelector('#dom');
-
-
-Loading.start({
-	dom:dom,
-	w:100,
-	h:100,
-    fillStyle: '#dd0',
-    strokeStyle:"#f00",
-    tc:"#d0d"	
-
-});
+require('../lib/Loading');
  //Loading.end();
-setTimeout(function(){
-  Loading.start({
-	dom:dom,
-	w:100,
-	h:100,
 
- });
-},6000)
-setTimeout(function(){
-  Loading.start({
-	dom:dom,
-	w:200,
-	h:200,
-
- });
-},6000)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var webpack=require('webpack');//插件
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+console.log(UglifyJSPlugin);
 module.exports={
 	entry:__dirname+'/src/index.js',//入口文件配置
 	output:{ //出口文件配置
        path:__dirname+'/docs',
-       filename:'buddle.js'
+       filename:'dlLoading.min.js'
 	},
 	devtool:'source-map',
 	module:{
@@ -12,6 +14,12 @@ module.exports={
 	},
 	plugins:[
 	   new webpack.BannerPlugin('delen 出品')
+	   // new webpack.optimize.UglifyJsPlugin({
+		  // minimize: true,
+		  // compress: {
+		  //   warnings: false,
+		  // },
+	   // })
 	],
 	//运行方式 webpack-dev-server --inline --hot修改代码自动刷新
 	// 
